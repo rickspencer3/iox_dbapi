edff95afaf17bd3575e8e7a16a45b3d7adba6b1d
Rick Spencer <rick@influxdata.com>
2022-11-09 08:51:00 -0500

    added bool to possible types

--- iox_dbapi.py
+++ iox_dbapi.py
@@ -56,6 +56,9 @@ class IOxCursor:
         desc = []
         if self._result is None:
             return None
+        if len(self._result) == 0:
+            return None
+            
         else:
             for column in self._result[self._table_index].columns:
                 desc.append((column.label,
@@ -70,8 +73,10 @@ class IOxCursor:
             return DataTypes.INTEGER
         elif type_string == "double":
             return DataTypes.FLOAT
-        elif type_string == "dateTime:RFC3339":
+        elif type_string.find("dateTime") > -1:
             return DataTypes.DATETIME
+        elif type_string == "bool":
+            return DataTypes.BOOL
 
     @property
     def rowcount(self):
@@ -137,37 +142,8 @@ class DataTypes(Enum):
     TIMESTAMP = 2
     INTEGER = 3
     FLOAT = 4
+    BOOL = 5
 
-
-
-
-# {0: 'DECIMAL',
-#  1: 'TINY',
-#  2: 'SHORT',
-#  3: 'LONG',
-#  4: 'FLOAT',
-#  5: 'DOUBLE',
-#  6: 'NULL',
-#  7: 'TIMESTAMP',
-#  8: 'LONGLONG',
-#  9: 'INT24',
-#  10: 'DATE',
-#  11: 'TIME',
-#  12: 'DATETIME',
-#  13: 'YEAR',
-#  14: 'NEWDATE',
-#  15: 'VARCHAR',
-#  16: 'BIT',
-#  246: 'NEWDECIMAL',
-#  247: 'INTERVAL',
-#  248: 'SET',
-#  249: 'TINY_BLOB',
-#  250: 'MEDIUM_BLOB',
-#  251: 'LONG_BLOB',
-#  252: 'BLOB',
-#  253: 'VAR_STRING',
-#  254: 'STRING',
-#  255: 'GEOMETRY'}
 
 
 # Date(year, month, day)
